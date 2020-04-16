101fa7f5bb7cb66992190b9a6edc08f95f610e88
zguang <zguang.loh@gmail.com>
2020-04-16 20:30:31 +0800

    Printing in Color

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Notes-App
+
+1. Importing Node.js Core Modules
+2. Importing Your Own Files
+3. Importing npm Modules
+4. Printing in Color
+5. Global npm Modules and nodemon
--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const validator = require("validator");
+const chalk = require("chalk");
 const getNotes = require("./notes");
 
 const msg = getNotes();
 
 console.log(msg);
 console.log(validator.isURL("https://mead.io"));
+console.log(chalk.green.bold("Success!"));
