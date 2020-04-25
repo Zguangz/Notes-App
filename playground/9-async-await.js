1f7d396ac30bb030fc13d20df1eb372a28eb876b
zguang <zguang.loh@gmail.com>
2020-04-25 17:58:51 +0800

    Async-Await

---
+++ playground/9-async-await.js
@@ -0,0 +1,32 @@
+const add = (a, b) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (a < 0 || b < 0) {
+        return reject("Numbers must be non-negative");
+      }
+      resolve(a + b);
+    }, 2000);
+  });
+};
+
+const doWork = async () => {
+  const sum = await add(1, 99);
+  console.log(sum);
+  const sum2 = await add(sum, 50);
+  console.log(sum2);
+  const sum3 = await add(sum2, -3);
+  console.log(sum3);
+  return sum3;
+};
+
+doWork()
+  .then((result) => {
+    console.log("result", result);
+  })
+  .catch((e) => {
+    console.log("error", e);
+  });
+
+// async function always return a promise
+// await can be used as Add function return a promise
+// async-await can make a async function look like a sync function call
